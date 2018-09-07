162f8f3df3120a0273973eb848b6b1efe5cb9e79
hottua <38776770+hottua@users.noreply.github.com>
2018-09-08 01:09:00 +0700

    UPdate manusia model
    Memperbaiki bugs

--- src/Manusia.java
+++ src/Manusia.java
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
 int main() {
+asdfasdfkjdf askjdfn askjdfn kasjdf
+asdfn as;djfkn as;dkljfna s;dfln asd;lfn asdlfkj 
+af asdlfknas;ldfjkn as;djklf nas
+f asldfknas 'dfasdlkn f
+a sdf;alskjdfn ;asldkfn as
+fas dfnkadslf 
 	return 0;
 }
